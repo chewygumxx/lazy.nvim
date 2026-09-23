8af4279496c6aeb2c2e78d94049324cbbd2c0426
chewygumxx <cgumxx@pm.me>
2026-09-23 11:49:22 +1000

    fix(view): Fix warnings and add headers

--- lua/lazy/view/diff.lua
+++ lua/lazy/view/diff.lua
@@ -1,3 +1,15 @@
+#!/usr/bin/env lua
+-- vim:set expandtab shiftwidth=4 filetype=lua:
+-- SPDX-License-Identifier: Apache-2.0
+
+--
+--
+-- ~folke/lazy.nvim.git
+-- └─> ~chewygumxx/lazy.nvim.git
+-- ::: :/lua/lazy/view/diff.lua
+--
+--
+
 local Util = require("lazy.util")
 
 local M = {}
@@ -7,58 +19,66 @@ local M = {}
 
 M.handlers = {
 
-  ---@type LazyDiffFun
-  browser = function(plugin, diff)
-    if plugin.url then
-      local url = plugin.url:gsub("%.git$", "")
-      if diff.commit then
-        Util.open(url .. "/commit/" .. diff.commit)
-      else
-        Util.open(url .. "/compare/" .. diff.from .. ".." .. diff.to)
-      end
-    else
-      Util.error("No url for " .. plugin.name)
-    end
-  end,
+    ---@type LazyDiffFun
+    browser = function(plugin, diff)
+        if plugin.url then
+            local url = plugin.url:gsub("%.git$", "")
+            if diff.commit then
+                Util.open(url .. "/commit/" .. diff.commit)
+            else
+                Util.open(url .. "/compare/" .. diff.from .. ".." .. diff.to)
+            end
+        else
+            Util.error("No url for " .. plugin.name)
+        end
+    end,
 
-  ---@type LazyDiffFun
-  ["diffview.nvim"] = function(plugin, diff)
-    local args
-    if diff.commit then
-      args = ("-C=%s"):format(plugin.dir) .. " " .. diff.commit .. "^!"
-    else
-      args = ("-C=%s"):format(plugin.dir) .. " " .. diff.from .. ".." .. diff.to
-    end
-    vim.cmd("DiffviewOpen " .. args)
-  end,
+    ---@type LazyDiffFun
+    ["diffview.nvim"] = function(plugin, diff)
+        local args
+        if diff.commit then
+            args = ("-C=%s"):format(plugin.dir) .. " " .. diff.commit .. "^!"
+        else
+            args = ("-C=%s"):format(plugin.dir) .. " "
+                .. diff.from .. ".."
+                .. diff.to
+        end
+        vim.cmd("DiffviewOpen " .. args)
+    end,
 
-  ---@type LazyDiffFun
-  git = function(plugin, diff)
-    local cmd = { "git" }
-    if diff.commit then
-      cmd[#cmd + 1] = "show"
-      cmd[#cmd + 1] = diff.commit
-    else
-      cmd[#cmd + 1] = "diff"
-      cmd[#cmd + 1] = diff.from
-      cmd[#cmd + 1] = diff.to
-    end
-    Util.float_cmd(cmd, { cwd = plugin.dir, filetype = "git" })
-  end,
+    ---@type LazyDiffFun
+    git = function(plugin, diff)
+        local cmd = { "git" }
+        if diff.commit then
+            cmd[#cmd + 1] = "show"
+            cmd[#cmd + 1] = diff.commit
+        else
+            cmd[#cmd + 1] = "diff"
+            cmd[#cmd + 1] = diff.from
+            cmd[#cmd + 1] = diff.to
+        end
+        Util.float_cmd(cmd, { cwd = plugin.dir, filetype = "git" })
+    end,
 
-  ---@type LazyDiffFun
-  terminal_git = function(plugin, diff)
-    local cmd = { "git" }
-    if diff.commit then
-      cmd[#cmd + 1] = "show"
-      cmd[#cmd + 1] = diff.commit
-    else
-      cmd[#cmd + 1] = "diff"
-      cmd[#cmd + 1] = diff.from
-      cmd[#cmd + 1] = diff.to
-    end
-    Util.float_term(cmd, { cwd = plugin.dir, interactive = false, env = { PAGER = "cat" } })
-  end,
+    ---@type LazyDiffFun
+    terminal_git = function(plugin, diff)
+        local cmd = { "git" }
+        if diff.commit then
+            cmd[#cmd + 1] = "show"
+            cmd[#cmd + 1] = diff.commit
+        else
+            cmd[#cmd + 1] = "diff"
+            cmd[#cmd + 1] = diff.from
+            cmd[#cmd + 1] = diff.to
+        end
+        Util.float_term(cmd, {
+            cwd = plugin.dir,
+            interactive = false,
+            env = {
+                PAGER = "cat",
+            },
+        })
+    end,
 }
 
 return M
