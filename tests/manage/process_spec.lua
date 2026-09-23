924802fa770ec3bb652fd13ae60ab14f138f7419
chewygumxx <cgumxx@pm.me>
2026-09-23 11:55:02 +1000

    test(manage): Fix warnings and add headers

--- tests/manage/process_spec.lua
+++ tests/manage/process_spec.lua
@@ -1,19 +1,20 @@
-local Async = require("lazy.async")
+local Async   = require("lazy.async")
 local Process = require("lazy.manage.process")
 
 describe("process", function()
-  it("runs sync", function()
-    local lines = Process.exec({ "echo", "-n", "hello" })
-    assert.are.same({ "hello" }, lines)
-  end)
-
-  it("runs sync from async context", function()
-    local lines ---@type string[]
-    local async = Async.new(function()
-      lines = Process.exec({ "echo", "-n", "hello" })
+    it("runs sync", function()
+        local lines = Process.exec({ "echo", "-n", "hello" })
+        assert.are.same({ "hello" }, lines)
     end)
-    async:wait()
 
-    assert.are.same({ "hello" }, lines)
-  end)
+    it("runs sync from async context", function()
+        ---@type string[]
+        local lines
+        local async = Async.new(function()
+            lines = Process.exec({ "echo", "-n", "hello" })
+        end)
+        async:wait()
+
+        assert.are.same({ "hello" }, lines)
+    end)
 end)
