8fff89d4cdcb72ea1e444cf9e24e971fbd146f8a
chewygumxx <cgumxx@pm.me>
2026-09-23 11:49:32 +1000

    fix(pkg): Fix warnings and add headers

--- lua/lazy/pkg/lazy.lua
+++ lua/lazy/pkg/lazy.lua
@@ -1,3 +1,15 @@
+#!/usr/bin/env lua
+-- vim:set expandtab shiftwidth=4 filetype=lua:
+-- SPDX-License-Identifier: Apache-2.0
+
+--
+--
+-- ~folke/lazy.nvim.git
+-- └─> ~chewygumxx/lazy.nvim.git
+-- ::: :/lua/lazy/pkg/lazy.lua
+--
+--
+
 local Util = require("lazy.util")
 
 local M = {}
@@ -7,23 +19,25 @@ M.lazy_file = "lazy.lua"
 ---@param plugin LazyPlugin
 ---@return LazyPkg?
 function M.get(plugin)
-  local file = Util.norm(plugin.dir .. "/" .. M.lazy_file)
-  if Util.file_exists(file) then
-    ---@type fun(): LazySpec
-    local chunk = Util.try(function()
-      local ret, err = loadfile(file)
-      return err and error(err) or ret
-    end, "`" .. M.lazy_file .. "` for **" .. plugin.name .. "** has errors:")
-    if not chunk then
-      Util.error("Invalid `" .. M.lazy_file .. "` for **" .. plugin.name .. "**")
-      return
+    local file = Util.norm(plugin.dir .. "/" .. M.lazy_file)
+    if Util.file_exists(file) then
+        ---@type fun(): LazySpec
+        local chunk = Util.try(function()
+            local ret, err = loadfile(file)
+            return err and error(err) or ret
+        end, "`" .. M.lazy_file .. "` for **" .. plugin.name .. "** has errors:")
+        if not chunk then
+            Util.error(
+                "Invalid `" .. M.lazy_file .. "` for **" .. plugin.name .. "**"
+            )
+            return
+        end
+        return {
+            source = "lazy",
+            file = M.lazy_file,
+            code = "function()\n" .. Util.read_file(file) .. "\nend",
+        }
     end
-    return {
-      source = "lazy",
-      file = M.lazy_file,
-      code = "function()\n" .. Util.read_file(file) .. "\nend",
-    }
-  end
 end
 
 return M
