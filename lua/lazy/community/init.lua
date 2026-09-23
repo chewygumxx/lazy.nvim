e5b83ad6d8631408b08f9464a1d35a07e4d9cfc0
chewygumxx <cgumxx@pm.me>
2026-09-23 11:49:43 +1000

    chore: Add header to community modules

--- lua/lazy/community/init.lua
+++ lua/lazy/community/init.lua
@@ -1,3 +1,15 @@
+#!/usr/bin/env lua
+-- vim:set expandtab shiftwidth=4 filetype=lua:
+-- SPDX-License-Identifier: Apache-2.0
+
+--
+--
+-- ~folke/lazy.nvim.git
+-- └─> ~chewygumxx/lazy.nvim.git
+-- ::: :/lua/lazy/community/init.lua
+--
+--
+
 local M = {}
 
 ---@type table<string, string>
@@ -5,27 +17,27 @@ local mapping = nil
 
 ---@return table<string, string>
 local function load()
-  if not mapping then
-    mapping = {}
-    ---@type {name:string, url:string, version:string}[]
-    local gen = require("lazy.community._generated")
-    for _, rock in ipairs(gen) do
-      mapping[rock.name] = rock.url
+    if not mapping then
+        mapping = {}
+        ---@type { name: string, url: string, version: string } []
+        local gen = require("lazy.community._generated")
+        for _, rock in ipairs(gen) do
+            mapping[rock.name] = rock.url
+        end
     end
-  end
-  return mapping
+    return mapping
 end
 
 ---@param rock string
 ---@return string?
 function M.get_url(rock)
-  return load()[rock]
+    return load()[rock]
 end
 
 ---@param name string
 ---@return LazyPluginSpec?
 function M.get_spec(name)
-  return require("lazy.community.specs")[name]
+    return require("lazy.community.specs")[name]
 end
 
 return M
--- lua/lazy/community/specs.lua
+++ lua/lazy/community/specs.lua
@@ -1,7 +1,19 @@
----@type table<string, LazySpec>
+#!/usr/bin/env lua
+-- vim:set expandtab shiftwidth=4 filetype=lua:
+-- SPDX-License-Identifier: Apache-2.0
+
+--
+--
+-- ~folke/lazy.nvim.git
+-- └─> ~chewygumxx/lazy.nvim.git
+-- ::: :/lua/lazy/community/specs.lua
+--
+--
+
+---@type table<string, LazyPluginSpec>
 return {
-  ["plenary.nvim"] = {
-    "nvim-lua/plenary.nvim",
-    lazy = true,
-  },
+    ["plenary.nvim"] = {
+        "nvim-lua/plenary.nvim",
+        lazy = true,
+    },
 }
