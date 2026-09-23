f6b11c6dafc6d00261b3b47d8f4075d66a6d5bd7
chewygumxx <cgumxx@pm.me>
2026-09-23 10:03:16 +1000

    test(runner): fix unrestored task stub leak and cover concurrency/sync branches

--- tests/manage/runner_spec.lua
+++ tests/manage/runner_spec.lua
@@ -1,4 +1,5 @@
 local Async = require("lazy.async")
+local Mocks = require("mocks")
 local Runner = require("lazy.manage.runner")
 
 describe("runner", function()
@@ -10,35 +11,44 @@ describe("runner", function()
     runs = {}
   end)
 
-  package.loaded["lazy.manage.task.test"] = {}
-  package.loaded["lazy.manage.task.test"]["skip"] = {
-    skip = function()
-      return true
-    end,
-  }
-  for i = 1, 10 do
-    package.loaded["lazy.manage.task.test"]["test" .. i] = {
-      ---@param task LazyTask
-      run = function(task)
-        table.insert(runs, { plugin = task.plugin.name, task = task.name })
-      end,
+  local restore_tasks
+  setup(function()
+    ---@type table<string, LazyTaskDef>
+    local defs = {
+      skip = {
+        skip = function()
+          return true
+        end,
+      },
     }
-    package.loaded["lazy.manage.task.test"]["error" .. i] = {
-      ---@param task LazyTask
-      run = function(task)
-        table.insert(runs, { plugin = task.plugin.name, task = task.name })
-        error("error" .. i)
-      end,
-    }
-    package.loaded["lazy.manage.task.test"]["async" .. i] = {
-      ---@async
-      ---@param task LazyTask
-      run = function(task)
-        Async.yield()
-        table.insert(runs, { plugin = task.plugin.name, task = task.name })
-      end,
-    }
-  end
+    for i = 1, 10 do
+      defs["test" .. i] = {
+        ---@param task LazyTask
+        run = function(task)
+          table.insert(runs, { plugin = task.plugin.name, task = task.name })
+        end,
+      }
+      defs["error" .. i] = {
+        ---@param task LazyTask
+        run = function(task)
+          table.insert(runs, { plugin = task.plugin.name, task = task.name })
+          error("error" .. i)
+        end,
+      }
+      defs["async" .. i] = {
+        ---@async
+        ---@param task LazyTask
+        run = function(task)
+          Async.yield()
+          table.insert(runs, { plugin = task.plugin.name, task = task.name })
+        end,
+      }
+    end
+    restore_tasks = Mocks.stub_tasks("test", defs)
+  end)
+  teardown(function()
+    restore_tasks()
+  end)
 
   it("runs the pipeline", function()
     local runner = Runner.new({ plugins = plugins, pipeline = { "test.test1", "test.test2" } })
@@ -79,6 +89,46 @@ describe("runner", function()
     local runner = Runner.new({ plugins = plugins, pipeline = { "test.test1", "test.error1", "test.test2" } })
     runner:start()
     runner:wait()
+    assert.equal(4, #runs)
+  end)
+
+  it("respects the concurrency limit", function()
+    local active = 0
+    local max_active = 0
+    package.loaded["lazy.manage.task.test"]["conc"] = {
+      ---@async
+      ---@param task LazyTask
+      run = function(_)
+        active = active + 1
+        max_active = math.max(max_active, active)
+        Async.yield()
+        active = active - 1
+      end,
+    }
+    local runner = Runner.new({ plugins = plugins, pipeline = { "test.conc" }, concurrency = 1 })
+    runner:start()
+    runner:wait()
+    assert.equal(1, max_active)
+  end)
+
+  it("runs the sync callback at a wait barrier", function()
+    local synced = false
+    local runner = Runner.new({
+      plugins = plugins,
+      pipeline = {
+        "test.test1",
+        {
+          "wait",
+          sync = function()
+            synced = true
+          end,
+        },
+        "test.test2",
+      },
+    })
+    runner:start()
+    runner:wait()
+    assert(synced)
     assert.equal(4, #runs)
   end)
 end)
